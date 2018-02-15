6636a97a87b16b35c8ed1b2f62541e129f803e0d
Austin Browne <aus.bro22@gmail.com>
2018-02-15 20:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-var name : "you";
-var holiday : "Independence Day": "Mother\'s Day";
-var days : 20;
-
 function happyHolidays(string) {
     return "Happy holidays!"
     }
 
 function happyHolidaysTo(name, holiday) {
     if(name) {
-      return "Happy holidays,"${name}"!"
+      return "Happy holidays,"${name}"!";
 } else if(name + holiday) {
-    return "Happy "${holiday}", "${name}"!"
+    return "Happy "${holiday}", "${name}"!";
   }
 
 }
