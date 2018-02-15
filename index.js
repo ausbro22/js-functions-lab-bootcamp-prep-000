7a8499296dabaf4bd5f22db9522b95e39a0362a2
Austin Browne <aus.bro22@gmail.com>
2018-02-15 20:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string) {
     }
 
 function happyHolidaysTo(name, holiday) {
-    if(you) {
+    if(name === you) {
       return "Happy holidays, ${name}!"
 } else if(name, holiday) {
     return "Happy ${holiday}, ${name}!"
