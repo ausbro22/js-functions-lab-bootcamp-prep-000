095e3cab26bd8ee785e819240d1c600c038359e8
Austin Browne <aus.bro22@gmail.com>
2018-02-15 20:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function happyHolidays(string) {
 function happyHolidaysTo(name, holiday) {
    if(holiday, name){
      return `Happy ${holiday}, ${name}!`
-   } 
+   }else if(name){
    
    return `Happy holidays, ${name}!`; 
+}
 }
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
